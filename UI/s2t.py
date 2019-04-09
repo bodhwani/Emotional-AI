3cfe004e0b05cfa115daecae223c2ae7901c9a1e
Vinit Bodhwani <vinit.bodhwani2015@vit.ac.in>
2019-04-09 18:29:07 +0530

    UI removed

--- UI/s2t.py
+++
@@ -1,6 +0,0 @@
-import speech_recognition as sr
-r = sr.Recognizer()
-harvard = sr.AudioFile('output.wav')
-with harvard as source:
-    audio = r.record(source)
-print("text is ",r.recognize_google(audio))
